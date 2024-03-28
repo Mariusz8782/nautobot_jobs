76d4a64bc962c02fcd917bc67ab49f5cb1cc7158
Mariusz8782 <amariusz82@gmail.com>
2024-03-28 12:46:51 +0100

    podmianka

--- jobs/__init__ copy.py
+++ jobs/__init__ copy.py
@@ -2,3 +2,7 @@ from nautobot.apps.jobs import register_jobs
 from .my_job import SimpleLogJob
 from .import_device_template import AddDeviceTypeJob
 register_jobs(SimpleLogJob, AddDeviceTypeJob)
+
+from nautobot.apps.jobs import register_jobs
+from .my_job import SimpleLogJob
+register_jobs(SimpleLogJob)
